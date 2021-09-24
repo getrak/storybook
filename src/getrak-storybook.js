6ce360a97b97ec71b11f80ebdb70db0cb471807b
Denys Rocha <denys.rocha@getrak.com.br>
2021-09-24 08:45:56 -0300

    Pimeiro commit

---
+++ src/getrak-storybook.js
@@ -0,0 +1,3 @@
+import { GetrakStorybook } from './GetrakStorybook.js';
+
+customElements.define('getrak-storybook', GetrakStorybook);
---
+++ src/stories/getrak-storybook.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-html';
+import '../getrak-storybook.js';
+
+export default {
+  title: 'GetrakStorybook',
+};
+
+export const App = () =>
+  html`
+    <getrak-storybook></getrak-storybook>
+  `;
