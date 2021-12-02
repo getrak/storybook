61548f52aed63da43bee7347e9afed81c774de10
Denys Rocha <denys.rocha@getrak.com.br>
2021-12-01 22:51:08 -0300

    Configurando o Chromatic

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -10,7 +10,7 @@ import * as designTokens from '../../design-tokens.tokens.json';
 
 const useStyles = createUseStyles({
   myClass: {
-    backgroundColor: designTokens.color.verdesucesso.value
+    backgroundColor: designTokens.color.amareloalerta.value
   }
 });
 
